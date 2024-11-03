66a9e22c267a7755d1ec08cd0b236939949d56a2
Ángel Gandarias <angel.gandarias@opendeusto.es>
2024-11-03 19:31:00 +0100

    -Añadida clase MenuPausa INCOMPLETA.

---
+++ PROYECTO/src/gui/MenuPausa.java
@@ -0,0 +1,62 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.time.LocalDate;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import main.ManejoTeclado;
+
+public class MenuPausa extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private ManejoTeclado mt;
+	public MenuPausa() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Menu pausa");
+		setSize(300, 400);
+		setResizable(false);
+		setLayout(new GridLayout(5, 1,0, 50));
+		setLocationRelativeTo(null);
+		//En el futuro, información puede contener información útil, como la celda actual, las coordenadas del jugador
+		//o incluso el objetivo principal actual
+		JLabel informacion = new JLabel("Información");
+		informacion.setHorizontalAlignment(SwingConstants.CENTER);
+		informacion.setFont(new Font(Font.DIALOG, Font.BOLD, 20));
+		
+		JButton reanudar = new JButton("Reanudar partida");
+		//Placeholder podría ser un menú de instrucciones, un menú de opciones o incluso un diario de objetivos 
+		JButton placeholder1 = new JButton("Placeholder");
+		JButton salir = new JButton("Salir del juego");
+		salir.addActionListener(e -> System.exit(0) );
+		salir.setBackground(Color.RED);
+		//Ayuda a que la parte de abajo no parezca muy vacía y puede ser interesante.
+		JLabel hora = new JLabel(""+LocalDate.now());
+		hora.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		add(informacion);
+		add(reanudar);
+		add(placeholder1);
+		add(salir);
+		add(hora);
+		
+		
+		
+		setVisible(true);
+		
+	}
+
+	
+	
+//	public static void main(String[] args) {
+//		new MenuPausa();
+//	}
+}
