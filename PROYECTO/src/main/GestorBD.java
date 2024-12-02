06f24c4b2b10ab90eb0ddbac5db2964c728b76f9
Jon Aingeru Lobato <j.lobato@opendeusto.es>
2024-12-02 01:11:55 +0100

    primeros pasitos de la base de datos

---
+++ PROYECTO/src/main/GestorBD.java
@@ -0,0 +1,41 @@
+package PruebasBaseDatas;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GestorBD {
+
+	protected static String DRIVER_NAME;
+	protected static String DATABASE_FILE;
+	protected static String CONNECTION_STRING;
+	
+	public GestorBD() {
+		
+		//Carga la configuracion de base de datos del fichero de propiedades
+		try (FileInputStream f = new FileInputStream("config/database.properties")){
+			//Carga el archivo
+			Properties p = new Properties();
+			p.load(f);
+			
+			//Tenemos los valores en p
+			DRIVER_NAME = p.getProperty("DRIVER_NAME");
+			DRIVER_NAME = p.getProperty("DATABASE_FILE");
+			DRIVER_NAME = p.getProperty("CONNECTION_STRING") + DATABASE_FILE;
+			
+			
+		} catch (IOException e) {
+			System.out.println("Error, no se pudo leer el fichero de la configuracion");
+		}
+		
+		try {
+			//Caragar el driver SQLite
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException ex) {
+			System.err.format("\n* Error al cargar el driver de BBDD: %s", ex.getMessage());
+			ex.printStackTrace();
+		}
+		
+		
+	}
+}
