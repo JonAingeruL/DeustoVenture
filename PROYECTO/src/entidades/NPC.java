a164b5aebb91025cf378e5948296ebf66d0e4723
Ane Lekue <ane.lecue@opendeusto.es>
2025-01-05 04:03:54 +0100

    clase npc con posiciones...

---
+++ PROYECTO/src/entidades/NPC.java
@@ -0,0 +1,41 @@
+package entidades;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import main.GamePanel;
+
+public class NPC {
+	GamePanel gp;
+    private int posX;
+    private int posY;
+    private String frase;
+
+    public NPC(int posX, int posY, String frase, GamePanel gp) {
+        this.posX = posX;
+        this.posY = posY;
+        this.frase = frase;
+        this.gp =gp;
+    }
+
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+    
+    public void dibujarNpc(Graphics2D g2) {
+		g2.setColor(new Color(0,0,0));
+
+		g2.fillRect(posX, posY, gp.tamañoBaldosa, gp.tamañoBaldosa);
+
+}
+
+}
