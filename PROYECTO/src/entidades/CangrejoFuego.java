22dbe6c8009fc7274ed01be42dbbacb1aabc1652
Marcos Dario Granda <marcosdario.granda@opendeusto.es>
2025-01-08 11:52:21 +0100

    Añadido el movimiento especial de dos enemigos

--- PROYECTO/src/entidades/CangrejoFuego.java
+++ PROYECTO/src/entidades/CangrejoFuego.java
@@ -1,6 +1,9 @@
 package entidades;
 
+import java.util.Random;
+
 import main.GamePanel;
+import main.Mapa;
 
 public class CangrejoFuego extends Enemigo {
 	
@@ -11,7 +14,40 @@ public class CangrejoFuego extends Enemigo {
 		this.velocidad = 5;
 		this.x = x;
 		this.y = y;
+		Random r = new Random();
+		this.direccion = r.nextInt(1,5);
 	}
 	
-
+	public void movimiento(Mapa mapa, int tamanoBaldosa, Jugador jugador) {
+		switch(this.direccion) {
+		case 1,3: x+=velocidad;
+		if(detectaColisionJugador(mapa, jugador, tamanoBaldosa)) {
+			jugador.cambiarVidas(-1);
+		}
+		while(detectaColisionJugador(mapa,jugador,tamanoBaldosa)) {
+			x-=1;
+		}
+		break;
+		case 2,4: x-=velocidad;
+		if(detectaColisionJugador(mapa,jugador, tamanoBaldosa)) {
+			jugador.cambiarVidas(-1);
+		}
+		while(detectaColisionJugador(mapa, jugador, tamanoBaldosa)) {
+			x+=1;
+		}
+		break;
+		}
+		if(detectaColision(mapa, tamanoBaldosa) || x<0 || x>16*tamanoBaldosa || y>12*tamanoBaldosa || y<0 ) {
+			switch(this.direccion) {
+			case 1,3: x-=velocidad;
+			break;
+			case 2,4: x+=velocidad;
+			break;
+			}
+			Random r= new Random();
+			direccion = r.nextInt(1,5);
+			
+		}
+	}
 }
+
--- PROYECTO/src/entidades/EspirituFuego.java
+++ PROYECTO/src/entidades/EspirituFuego.java
@@ -1,6 +1,9 @@
 package entidades;
 
+import java.util.Random;
+
 import main.GamePanel;
+import main.Mapa;
 
 public class EspirituFuego extends Enemigo {
 	
@@ -11,6 +14,39 @@ public class EspirituFuego extends Enemigo {
 		this.velocidad = 5;
 		this.x = x;
 		this.y = y;
+		Random r = new Random();
+		this.direccion = r.nextInt(1,5);
+	}
+	
+	public void movimiento(Mapa mapa, int tamanoBaldosa, Jugador jugador) {
+		switch(this.direccion) {
+		case 1,3: y+=velocidad;
+		if(detectaColisionJugador(mapa, jugador, tamanoBaldosa)) {
+			jugador.cambiarVidas(-1);
+		}
+		while(detectaColisionJugador(mapa, jugador, tamanoBaldosa)) {
+			y-=1;
+		}
+		break;
+		case 2,4: y-=velocidad;
+		if(detectaColisionJugador(mapa, jugador, tamanoBaldosa)) {
+			jugador.cambiarVidas(-1);
+		}
+		while(detectaColisionJugador(mapa, jugador, tamanoBaldosa)) {
+			y+=1;
+		}
+		break;
+		}
+		if(detectaColision(mapa, tamanoBaldosa) || x<0 || x>16*tamanoBaldosa || y>12*tamanoBaldosa || y<0 ) {
+			switch(this.direccion) {
+				case 1,3: y-=velocidad;
+				break;
+				case 2,4: y+=velocidad;
+				break;
+			}
+			Random r = new Random();
+			direccion = r.nextInt(1,5);
+		}
 	}
 
 }
